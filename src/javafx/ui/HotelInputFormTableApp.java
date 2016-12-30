29c6a5562633dca698600915502293ed48e88036
Miroslav Metodiev <miroslav_metodiev@epam.com>
2016-12-30 15:11:52 +0200

    Create datepicker

--- src/javafx/ui/HotelInputFormTableApp.java
+++ src/javafx/ui/HotelInputFormTableApp.java
@@ -1,26 +1,43 @@
 package javafx.ui;
 
-import javax.swing.text.html.AccessibleHTML.TableElementInfo.TableAccessibleContext;
-
+//see this blog for more info http://code.makery.ch/blog/javafx-dialogs-official/
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.database.HotelFormDataAccessor;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.model.HotelFormModel;
 import javafx.model.Person;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class HotelInputFormTableApp extends Application {
 
 	private HotelFormDataAccessor dataAccessor;
 
+	private final ObservableList<Person> data =
+            FXCollections.observableArrayList(
+            new Person("Jacob", "Smith", "jacob.smith@example.com"),
+            new Person("Isabella", "Johnson", "isabella.johnson@example.com"),
+            new Person("Ethan", "Williams", "ethan.williams@example.com"),
+            new Person("Emma", "Jones", "emma.jones@example.com"),
+            new Person("Michael", "Brown", "michael.brown@example.com"));
+	
+	
+	@SuppressWarnings("unchecked")
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// String driverClassName, String dbURL, String user, String password
@@ -73,17 +90,35 @@ public class HotelInputFormTableApp extends Application {
                         addFirstName.getText(),
                         addLastName.getText(),
                         addEmail.getText()));
+                
+                Alert alert = new Alert(AlertType.INFORMATION);
+                alert.setTitle("Information Dialog");
+                alert.setHeaderText(null);
+                alert.setContentText(addEmail.getText());
+
+                alert.showAndWait();
+                
                 addFirstName.clear();
                 addLastName.clear();
                 addEmail.clear();
             }
         });
- 
-		
+        
+        HBox hb = new HBox();
+        
+        hb.getChildren().addAll(addFirstName, addLastName, addEmail, addButton);
+        hb.setSpacing(3);
+        
+        final VBox vbox = new VBox();
+        vbox.setSpacing(5);
+        vbox.setPadding(new Insets(10, 0, 0, 10));
+        vbox.getChildren().addAll(hotelModelTable, hb);
 		
 		BorderPane root = new BorderPane();
-		root.setCenter(hotelModelTable);
-		Scene scene = new Scene(root, 600, 400);
+		//root.setCenter(hotelModelTable);
+		Scene scene = new Scene(new Group());
+		((Group) scene.getRoot()).getChildren().addAll(vbox);
+		
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
